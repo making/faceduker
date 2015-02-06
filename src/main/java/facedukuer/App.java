fa9e3402a7ef7d8ea45c8efcb9fc560873351841
Toshiaki Maki <makingx@gmail.com>
2015-02-07 07:21:21 +0900

    support JMS

--- src/main/java/facedukuer/App.java
+++ src/main/java/facedukuer/App.java
@@ -7,9 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.JmsProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,10 +25,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import javax.imageio.ImageIO;
+import javax.jms.ConnectionFactory;
 import javax.servlet.http.Part;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.function.BiConsumer;
 
 import static org.bytedeco.javacpp.opencv_core.*;
@@ -35,10 +46,23 @@ public class App {
 
     @Autowired
     FaceDetector faceDetector;
+    @Autowired
+    JmsMessagingTemplate jmsTemplate;
+
+    static final Logger log = LoggerFactory.getLogger(App.class);
 
     @Bean
     BufferedImageHttpMessageConverter bufferedImageHttpMessageConverter() {
         return new BufferedImageHttpMessageConverter();
+    }
+
+    @Bean
+    DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, JmsProperties jmsProperties) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setPubSubDomain(jmsProperties.isPubSubDomain());
+        factory.setConcurrency("3-20");
+        return factory;
     }
 
     @RequestMapping(value = "/")
@@ -48,24 +72,34 @@ public class App {
 
     // curl -v -F 'file=@hoge.jpg' http://localhost:8080/duker > after.jpg
     @RequestMapping(value = "/duker", method = RequestMethod.POST)
-    BufferedImage duker(@RequestParam Part file) throws IOException {
-        Mat source = Mat.createFrom(ImageIO.read(file.getInputStream()));
-        faceDetector.detectFaces(source, FaceTranslator::duker);
-        BufferedImage image = new BufferedImage(source.cols(), source.rows(), source
-                .getBufferedImageType());
-        source.copyTo(image);
-        return image;
+    String duker(@RequestParam Part file) throws IOException {
+        byte[] src = StreamUtils.copyToByteArray(file.getInputStream());
+        jmsTemplate.convertAndSend("processImage", src);
+        return "OK";
+    }
+
+    @JmsListener(destination = "processImage")
+    void handleImageMessage(Message<byte[]> message) throws IOException {
+        log.info("received! {}", message);
+        try (InputStream stream = new ByteArrayInputStream(message.getPayload())) {
+            Mat source = Mat.createFrom(ImageIO.read(stream));
+            faceDetector.detectFaces(source, FaceTranslator::duker);
+            BufferedImage image = new BufferedImage(source.cols(), source.rows(), source
+                    .getBufferedImageType());
+            source.copyTo(image);
+        }
     }
 }
 
 @Component
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 class FaceDetector {
     @Value("${classifierFile:classpath:/haarcascade_frontalface_default.xml}")
     File classifierFile;
 
     CascadeClassifier classifier;
 
-    Logger log = LoggerFactory.getLogger(FaceDetector.class);
+    static final Logger log = LoggerFactory.getLogger(FaceDetector.class);
 
     public void detectFaces(Mat source, BiConsumer<Mat, Rect> detectAction) {
         Rect faceDetections = new Rect();
